c5009499d33043d31cedef7aef79ab1e2f357bd4
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-04-01 09:18:35 -0600

    list fleet method implemented, issue with read needs to be solved, quit method implemented

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -6,65 +6,53 @@ import com.skilldistillery.jets.entities.AirField;
 import com.skilldistillery.jets.entities.Jet;
 
 public class JetsApplication {
-	
+
 	private Scanner sc = new Scanner(System.in);
-	
-	//is this correct? should Jet be an array list?
+
+	// is this correct? should Jet be an array list?
 	private Jet[] fleet = new Jet[10];
-	
-	
+
 	public static void main(String[] args) {
 		JetsApplication ja = new JetsApplication();
 		ja.launch();
 	}
-	
+
 	public void launch() {
-		AirField af = new AirField();
-		af.readFromFile();
-		
-		
-	
+//		AirField af = new AirField();
+//
+//		af.readFromFile();
+
+		menu();
 	}
-	
-	public void exit() {
-		System.out.println("GoodBye!");
-	}
-	
+
 	public void menu() {
 		boolean running = true;
-		while(running) {
-			System.out.println(""
-					+ "\n 1. List Fleet "
-					+ "\n 2. Fly All Jets"
-					+ "\n 3. View Fastest Jet"
-					+ "\n 4. View Jet With Longest Range"
-					+ "\n 5. Load All Cargo Jets"
-					+ "\n 6. Dogfight!"
-					+ "\n 7. Add a Jet to the Fleet"
-					+ "\n 8. Remove a Jet from the Fleet"
-					+ "\n 9. Quit"
+		while (running) {
+			System.out.println("CHOOSE AN OPTION AND PRESS \" ENTER \"" + "\n 1. List Fleet " + "\n 2. Fly All Jets" + "\n 3. View Fastest Jet"
+					+ "\n 4. View Jet With Longest Range" + "\n 5. Load All Cargo Jets" + "\n 6. Dogfight!"
+					+ "\n 7. Add a Jet to the Fleet" + "\n 8. Remove a Jet from the Fleet" + "\n 9. Quit" 
 					);
 			int choice = sc.nextInt();
 			sc.nextLine();
-			switch(choice) {
+			switch (choice) {
 			case 1:
-				//list fleet
+				// list fleet
 				listFleet();
 				break;
 			case 2:
-				//Fly all jets
+				// Fly all jets
 				flyAll();
 				break;
 			case 3:
-				//View Fastest jet
+				// View Fastest jet
 				fastest();
 				break;
 			case 4:
-				//View jet with longest range
+				// View jet with longest range
 				longestRange();
 				break;
-			case 5: 
-				//Load all Cargo Jets
+			case 5:
+				// Load all Cargo Jets
 				loadCargoJets();
 				break;
 			case 6:
@@ -87,36 +75,56 @@ public class JetsApplication {
 				System.out.println("invalid entry.");
 			}
 		}
-		
+
 	}
+
 	public void listFleet() {
-		
+		// sysout id, model, speed, range, and price
+		AirField af = new AirField();
+		af.readFromFile();
 	}
+
 	public void flyAll() {
-		
+		// will call fly() an print the details of each jet
+		// prints ammount of time a jet can fly
+		// equation based on speed and range
 	}
+
 	public void fastest() {
-		
+		// search the list<Jets> to find fastest
 	}
+
 	public void longestRange() {
-		
+		// search list<Jets> to find longest range
 	}
+
 	public void loadCargoJets() {
-		
+		// present option specific to CargoCarrier Interface
+		// find all implementations of cargoCarrier
+		// call loadCargo() on each
 	}
+
 	public void dogfight() {
-		
+		// same as above loadCargoJets method but interface CombatReady
 	}
+
 	public void addJet() {
-		System.out.println("What type of jet would you like to add : ");
 		// print current list : listFleet();
-		//
-		//fleet.add
-		
+		// can be a JetImpl (passenger)
+		// STRETCH GOAL : Sub Menu initate to choose type of jet (Cargo, Combat, Impl)
+		// this process will be similar to foodtruck app
+		// ask user to enter model, speed, range, and price
+		// add new jet to jets.txt and list<Jet>
+		// fleet.add
 	}
+
 	public void removeJet() {
-	
+		// print current list as a submenu;
+		// prompt user to choose a jet to delte by number
 	}
-	
+
+	public void exit() {
+		System.out.println("GoodBye!");
+	}
 
 }
